44970c48efc839ebab4c26d2bff7b458e7b27d97
Patrick Bigler <patrick@macbook-pro-1.home>
2023-09-30 09:55:04 +0200

    update to the combined function

--- R/function_to_create_Combined.R
+++ R/function_to_create_Combined.R
@@ -1,7 +1,7 @@
 packages <- c("influxdbclient","ggplot2","tidyverse","lubridate","raster",
               "dplyr","googledrive","caret","rgdal","keras")
 
-source("../R/load_packages.R")
+source("../AGDS_Bigler_Tinner/R/load_packages.R")
 load_packages(packages)
 
 
@@ -14,17 +14,17 @@ looking_for_Combined.csv <- function(){
   if (!file.exists(name.of.file)){
 
     # Data from urban climate network of the city of Bern
-    measurement_files <- list.files("../data/Measurements/")
-    measurement_files <- purrr::map(as.list(paste("../data/Measurements/",measurement_files,sep = "")),~read_csv(.))
+    measurement_files <- list.files("../AGDS_Bigler_Tinner/data/Measurements/")
+    measurement_files <- purrr::map(as.list(paste("../AGDS_Bigler_Tinner/data/Measurements/",measurement_files,sep = "")),~read_csv(.))
     measurement_files <- bind_rows(measurement_files, .id = "column_label")
     measurement_files <-  measurement_files |> pivot_longer(cols = starts_with("L"), names_to = "Log_Nr",values_to = "temperature") |>
       mutate(Log_Nr = as.numeric(str_replace(Log_Nr, "Log_", "")))
     measurement_files <-  mutate(measurement_files,time = mdy_hm(Zeit)) |>
       dplyr::select(-Zeit)
-    measurement_metadata <- read_csv("../data/Metadata_19-22.csv")
+    measurement_metadata <- read_csv("../AGDS_Bigler_Tinner/data/Metadata_19-22.csv")
 
     # Data from Meteo Schweiz
-    meteoswiss <- read_csv2("../data/Meteoswiss/order_114596_data.txt")
+    meteoswiss <- read_csv2("../AGDS_Bigler_Tinner/data/Meteoswiss/order_114596_data.txt")
     meteoswiss <- meteoswiss |>
       mutate(time = as.POSIXct(as.character(time), format = "%Y%m%d%H%M"))
     meteoswiss_names <-meteoswiss|>
@@ -40,7 +40,7 @@ looking_for_Combined.csv <- function(){
 
     for (file in tiff_names){
       # Read the TIF file
-      raster_data <- raster::raster(paste("../data/Tiffs/",file,sep = ""))
+      raster_data <- raster::raster(paste("../AGDS_Bigler_Tinner/data/Tiffs/",file,sep = ""))
       # Extract values at the points
       points_values <- raster::extract(raster_data, combined[, c("LV_03_E", "LV_03_N")])
       # Add the extracted values to the points_table
@@ -48,12 +48,17 @@ looking_for_Combined.csv <- function(){
       print(file)
     } # End of the for loop
 
-    write_csv(combined,"../data/Combined.csv")
+    write_csv(combined,"../AGDS_Bigler_Tinner/data/Combined.csv")
+
+    Combined <- read_csv("../AGDS_Bigler_Tinner/data/Combined.csv")
+
+    return(Combined)
 
     # If exists such a file, read it only!
-  }else {Combined <- read_csv("../data/Combined.csv")
+  }else {Combined <- read_csv("../AGDS_Bigler_Tinner/data/Combined.csv")
+    return(Combined)
   } # end of the if/else statement
-  return(Combined)
+
 } # end of the function
 
 Combined <- looking_for_Combined.csv()
