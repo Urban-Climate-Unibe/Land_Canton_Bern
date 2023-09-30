bc9611636fb95744f0b05d4b8126968ea6ffbf21
Patrick Bigler <patrick@macbook-pro-1.home>
2023-09-30 09:50:58 +0200

    I added a new function to:
    create, write and read Combined if there is not such file or read Combined if there already such a file

---
+++ R/function_to_create_Combined.R
@@ -0,0 +1,59 @@
+packages <- c("influxdbclient","ggplot2","tidyverse","lubridate","raster",
+              "dplyr","googledrive","caret","rgdal","keras")
+
+source("../R/load_packages.R")
+load_packages(packages)
+
+
+# We want to know, if a certain file already exists
+looking_for_Combined.csv <- function(){
+
+  name.of.file <- '../data/Combined.csv'
+
+  # If do not exists such a file, create it!
+  if (!file.exists(name.of.file)){
+
+    # Data from urban climate network of the city of Bern
+    measurement_files <- list.files("../data/Measurements/")
+    measurement_files <- purrr::map(as.list(paste("../data/Measurements/",measurement_files,sep = "")),~read_csv(.))
+    measurement_files <- bind_rows(measurement_files, .id = "column_label")
+    measurement_files <-  measurement_files |> pivot_longer(cols = starts_with("L"), names_to = "Log_Nr",values_to = "temperature") |>
+      mutate(Log_Nr = as.numeric(str_replace(Log_Nr, "Log_", "")))
+    measurement_files <-  mutate(measurement_files,time = mdy_hm(Zeit)) |>
+      dplyr::select(-Zeit)
+    measurement_metadata <- read_csv("../data/Metadata_19-22.csv")
+
+    # Data from Meteo Schweiz
+    meteoswiss <- read_csv2("../data/Meteoswiss/order_114596_data.txt")
+    meteoswiss <- meteoswiss |>
+      mutate(time = as.POSIXct(as.character(time), format = "%Y%m%d%H%M"))
+    meteoswiss_names <-meteoswiss|>
+      dplyr::select(-stn)|>
+      colnames()
+
+    combined = inner_join(measurement_files,meteoswiss,by = "time")
+    combined = inner_join(combined, measurement_metadata, by = "Log_Nr")
+
+    tiff_names <- list.files("../data/Tiffs/")
+    tiff_names_short <- tiff_names |>
+      str_sub(end = -5)
+
+    for (file in tiff_names){
+      # Read the TIF file
+      raster_data <- raster::raster(paste("../data/Tiffs/",file,sep = ""))
+      # Extract values at the points
+      points_values <- raster::extract(raster_data, combined[, c("LV_03_E", "LV_03_N")])
+      # Add the extracted values to the points_table
+      combined[paste(str_sub(file,end = -5))] <- points_values
+      print(file)
+    } # End of the for loop
+
+    write_csv(combined,"../data/Combined.csv")
+
+    # If exists such a file, read it only!
+  }else {Combined <- read_csv("../data/Combined.csv")
+  } # end of the if/else statement
+  return(Combined)
+} # end of the function
+
+Combined <- looking_for_Combined.csv()
