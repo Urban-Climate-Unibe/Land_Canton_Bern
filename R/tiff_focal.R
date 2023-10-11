cd54bf751370dd3e4371d58672afdf77850e9c1c
Nils <slin@stud-207-97.eduroam.unibe.ch>
2023-10-11 13:28:50 +0200

    Added new implementations for data wrangling and so forth

---
+++ R/tiff_focal.R
@@ -0,0 +1,21 @@
+library(raster)
+library(stringr)
+
+
+files <- list.files("./data-raw/")
+
+for (file in files) {
+
+
+raster_data <- raster(paste("./data-raw/",file,sep = ""))
+
+raster_data <- reclassify(raster_data, cbind(NA, 0))
+
+for (x in c(5,11,21,51,101,201)) {
+  mean_focal <- focal(raster_data, w=matrix(1, nrow=x, ncol=x), fun=mean, na.rm=TRUE)
+  filename1 = paste("./data/Tiffs/",str_sub(file, end = -5),"_",x,".tif",sep = "")
+  print(filename1)
+  writeRaster(mean_focal, filename=filename1, format="GTiff")
+}
+
+}
