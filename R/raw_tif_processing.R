3ddc2c909f6d0c11434967c40dadafac38ced2b2
Nils <slin@stud-207-97.eduroam.unibe.ch>
2023-11-06 14:40:21 +0100

    Added autom. tiff processing for Land classifications, more to follow...

--- R/raw_tif_processing.R
+++ R/raw_tif_processing.R
@@ -1,10 +1,9 @@
-library(terra)
 
-options(timeout=2000)
-download.file("https://geofiles.be.ch/geoportal/pub/download/MOPUBE/MOPUBE.zip",destfile = "./data-raw/mopu.zip")
+options(timeout=4000)
+download.file("https://geofiles.be.ch/geoportal/pub/download/MOPUBE/MOPUBE.zip",destfile = paste0(tempdir(),"mopu.zip"))
 
 
-unzip("./data-raw/mopu.zip",exdir = "./data-raw/mopu")
+unzip(paste0(tempdir(),"mopu.zip"),exdir = paste0(tempdir(),"mopu"))
 
 
 
@@ -12,17 +11,16 @@ unzip("./data-raw/mopu.zip",exdir = "./data-raw/mopu")
 
 
 #Todo:
-#1. cut and make raster
-#2. convert calsses/aggregate
-#3. buffer -> done
+#building height
+#DEM
+#slope
+#svf
+#VH
 
-
-library(sf)
-library(stars)
 library(dplyr)
-library(terra)
+
 # Read in the shapefile
-shapefile <- st_read("./data-raw/mopu/data/MOPUBE_BBF.shp")
+shapefile <- st_read(paste0(tempdir(),"mopu/data/MOPUBE_BBF.shp"))
 
 # Define the extent you want to cut to
 # Replace these values with your desired extent (xmin, xmax, ymin, ymax)
@@ -33,9 +31,9 @@ shapefile_cropped <- st_crop(shapefile, extent_to_cut)
 
 
 
-st_rasterize(shapefile_cropped,file = "./data-raw/mopu/data/MOPUBE_BBF.tif",dx = 5, dy = 5) #resolution of 5 meters
+st_rasterize(shapefile_cropped,file = paste0(tempdir(),"mopu/data/MOPUBE_BBF.tif"),dx = 5, dy = 5) #resolution of 5 meters
 
-raster <- terra::rast("./data-raw/mopu/data/MOPUBE_BBF.tif")
+raster <- terra::rast(paste0(tempdir(),"mopu/data/MOPUBE_BBF.tif"))
 raster <- raster[[1]]
 
 land_classes <- dplyr::tibble(Art = shapefile$ART, Bez = shapefile$BBARTT_BEZ)
@@ -43,5 +41,90 @@ land_classes <- land_classes |> group_by(Art,Bez) |>
   summarise()
 
 
-raster %in% c(0,1,2)*1 #works!: all with 0,1,2 get a 1 else 0
+
+
+
+
+classification <- tibble(
+  Number_raw_data = 0:25,
+  Description_raw_data = c(
+    "Gebäude", "Strasse Weg", "Trottoir", "Verkehrsinsel", "Bahn", "Flugplatz", "Wasserbecken",
+    "Übrige befestigte", "Acker Wiese Weide", "Reben", "Übrige Intensivkulturen", "Gartenanlage",
+    "Hoch- Flachmoor", "Übrige humusierte", "Stehendes Gewässer", "Fliessenden Gewässer",
+    "Schilfgürtel", "Geschlossener Wald", "Wytweide dicht", "Wytweide offen", "Übrige bestockte",
+    "Fels", "Gletscher Firn", "Geröll Sand", "Abbau Deponie", "Übrige Vegetationslose"
+  ),
+  Reclassified_category = c(
+    "Land Cover Building", "Open Space Sealed", "Open Space Sealed", "Open Space Sealed",
+    "Open Space Sealed", "Open Space Sealed", "Open Space Water", "Open Space Sealed",
+    "Open Space Agriculture", "Open Space Agriculture", "Open Space Agriculture", "Open Space Garden",
+    "Did not appear", "Open Space Agriculture", "Open Space Water", "Open Space Water",
+    "Open Space Water", "Open Space Forest", "Did not appear", "Did not appear", "Open Space Forest",
+    "Open Space Sealed", "Did not appear", "Open Space Sealed", "Open Space Sealed", NA
+  )
+) |> tidyr::drop_na()
+
+meters <- tibble(
+  Reclassified_category = c("Land Cover Building", "Open Space Sealed", "Open Space Forest", "Open Space Garden", "Open Space Water", "Open Space Agriculture"),
+  Variable = c("LC_B", "OS_SE", "OS_FO", "OS_GA", "OS_WA", "OS_AC"),
+  Abbreviation = rep("25/50/150/250/500", 6),
+  Buffer_radii_tested = rep("25/50/150/250/500", 6),
+  Unit = rep("%", 6),
+  Chosen_buffer_radiusm = c(250, 500, 250, 25, 150, 500)
+)
+classification <- inner_join(classification,meters, by = "Reclassified_category")
+
+
+for (class in unique(classification$Reclassified_category)) {
+number_classes <- classification |>
+    filter(Reclassified_category == class) |>
+    dplyr::select(Number_raw_data) |>
+  unlist()
+print(class)
+print(number_classes)
+temp_raster <- raster %in% number_classes*1
+name <- classification |>
+  filter(Reclassified_category == class) |>
+  dplyr::select(Variable) |>
+  dplyr::group_by(Variable) |>
+  summarise()|>
+  unlist()
+
+terra::writeRaster(temp_raster,paste0("../data-raw/",name,".tif"),overwrite = T)
+
+}
+
+
+
+# Creating the tibble
+
+
+
+
+
+
+source("../R/tiff_focal.R")
+
+
+files <- list.files("../data-raw/")
+
+for (file in files) {
+
+
+  raster_data <- rast(paste("../data-raw/",file,sep = ""))
+
+  print(file)
+  print(str_sub(file,end = -5))
+  meter <- classification |>
+    filter(Variable == str_sub(file,end = -5)) |>
+    dplyr::select(Chosen_buffer_radiusm) |>
+    dplyr::group_by(Chosen_buffer_radiusm)|>
+    summarise()|>
+    unlist()
+print(meter)
+  tiff_focal(raster_data,meter)
+
+}
+
+
 
--- R/tiff_focal.R
+++ R/tiff_focal.R
@@ -1,20 +1,27 @@
-library(raster)
+tiff_focal <- function(tiff_path,meter){
+
 library(stringr)
 
 
-files <- list.files("./data-raw/")
-
-for (file in files) {
 
 
-raster_data <- raster(paste("./data-raw/",file,sep = ""))
 
-raster_data <- reclassify(raster_data, cbind(NA, 0))
-
-for (x in c(5,11,21,51,101,201)) {
-  mean_focal <- focal(raster_data, w=matrix(1, nrow=x, ncol=x), fun=mean, na.rm=TRUE)
-  filename1 = paste("./data/Tiffs/",str_sub(file, end = -5),"_",x,".tif",sep = "")
+  n <-2 * round(meter/2)/5+1 #get unequal number, div by 5 since 5m resolution
+print(n)
+  mean_focal <- focal(raster_data, w=matrix(1, nrow=n, ncol=n), fun=mean, na.rm=TRUE)
+  filename1 = paste("../data/Tiffs/",str_sub(file, end = -5),"_",meter,".tif",sep = "")
   print(filename1)
-  writeRaster(mean_focal, filename=filename1, format="GTiff")
-    }
+  writeRaster(mean_focal, filename=filename1,overwrite = T)
+
+
+
+
 }
+
+
+
+
+
+
+
+
