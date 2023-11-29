14e98c70c57ffe91c9bb4d4e7f43f419797ae9a4
Patrick Bigler <patrick@stud-86-54.eduroam.unibe.ch>
2023-11-29 09:34:06 +0100

    small update

--- R/example_map.R
+++ R/example_map.R
@@ -1,18 +1,19 @@
 
 
-
 meteoswiss <- c(22,0,0,0,0,30,27,23,21,20,0,0,0,0,0)
 
 meteoswiss_selection <- paste(c("temp","precip","rad","wind","rain"), collapse = "|")
 names(meteoswiss) <- combined |>
   dplyr::select(matches(meteoswiss_selection),
-                -temperature) |>
+                -c(temperature,sum_precipitation_10_days)) |>
   colnames()
-# Goes:""temp"                       "rad"                        "winds"                      "windd"                      "mean_temp_6_hours"    "mean_temp_12_hours"         "mean_temp_1_day"            "mean_temp_3_days"           "mean_temp_5_days"           "sum_precipitation_6_hours" "sum_precipitation_12_hours" "sum_precipitation_1_day"    "sum_precipitation_3_days"   "sum_precipitation_5_days"
+
+# Goes:""temp" "rad" "winds" "windd" "mean_temp_6_hours" "mean_temp_12_hours" "mean_temp_1_day"
+# "mean_temp_3_days" "mean_temp_5_days" "sum_precipitation_6_hours" "sum_precipitation_12_hours"
+# "sum_precipitation_1_day" "sum_precipitation_3_days" "sum_precipitation_5_days"
 
 
-
-tiffs<-tiffs_only
+tiffs <- tiffs_only
 
 
 for (name_var in names(meteoswiss)) {
@@ -22,22 +23,14 @@ for (name_var in names(meteoswiss)) {
   temp <- crop(temp,tiffs)
   temp <- resample(temp,tiffs)
   tiffs <- c(tiffs,temp)
-
-
 }
-
-
-
-
-
 
 
 temperature <- terra::predict(tiffs,random_forest_model,na.rm = T)
 
-
 extent <- rgdal::readOGR("../data/Map/Extent_Bern.shp")
 rivers <- rgdal::readOGR("../data/Map/Aare.shp")
-color = colorRampPalette(c("blue","deepskyblue", "white","orange", "red"))(100)
+color = colorRampPalette(c("blue", "white", "red"))(20)
 terra::plot(temperature,color = color)
 sp::plot(extent, add = T)
 sp::plot(rivers, add = T)
